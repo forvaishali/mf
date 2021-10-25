c53fc1b937cae2478473e3bbba158822cc733b84
vaishali jain <createdbyvj@gmail.com>
2021-10-25 08:40:39 +0530

    Added details to farming

--- src/components/farming.tsx
+++ src/components/farming.tsx
@@ -1,7 +1,39 @@
 import { Fragment } from "react";
 
 const Farming = () => {
-    return <Fragment><p>This is Farming portal</p></Fragment>
+    return <Fragment>
+         <div className="fcontainer">
+       
+        <div className="fitem item1">
+        <h2>Permaculture Food Forest</h2>
+        <h3>Permaculture is a way to grow variety of Fruits, Vegetables and Flowers in the small area. This preserves our environment and gives a healthy growth to the plants and creates a positive aura</h3></div>
+        <div className="fitem item2"><h2>Growing native plants</h2>
+        <h3> India is a country with lots of medicinal plants for our well-being. We need to focus on growing variety of plants like
+             Tulasi, aloevera, Babul, Neem, Giloy which are good for environment and help us cure 
+             lots of diseases and purity the air around us.</h3></div>
+        <div className="fitem item3">
+        <h2>Companion Planting</h2>
+        <h3>Do you want to avoid using harmful pesticides in your Garden or Farm. Go for companion planting. for example:
+            Marigold is a very good pest repeller. Corn plant provides shade to the Peas plant.</h3></div>
+        <div className="fitem item4"><h2>Food Preservation</h2>
+        <h3>While we give importance to growing and eating food. Storing Grains, Fruits and vegetables, cooked food in correct way
+            is very important for their long term shelf life and to avoid wastage of resources like: water, manure, time used in growing food.
+            </h3></div>
+        <div className="fitem item5"><h2>Agricuture subsidies</h2>
+        <h3>Government of India provides different subsidies for farmers to encourage farming and help
+            with cutting costs. Subsidies are available for agriculture equipment, plantation of native trees, Solar, Rain water harvesting.
+</h3>
+        </div>
+        <div className="fitem item5"><h2>Leaders in Farming</h2>
+        <h3><ul>
+            <li>Subhash Palekar</li>
+            <li>Akash Chaurasiiya</li>
+            <li>Dr. Vandana Shiva</li>
+
+<li>Manisha Lath Gupta (Aanandaa Permaculture Farm)</li>            </ul></h3></div>
+    </div>
+   
+            </Fragment>
         
 }
 export default Farming;
